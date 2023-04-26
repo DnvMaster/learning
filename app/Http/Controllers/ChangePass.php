397378c1517a956b85e3d29955ede664d0ff0e74
DnvMaster <dnvmaster@yandex.ru>
2023-04-26 21:41:18 +0300

    Created method UpdatePassword & change password

--- app/Http/Controllers/ChangePass.php
+++ app/Http/Controllers/ChangePass.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ChangePass extends Controller
 {
@@ -10,8 +13,23 @@ class ChangePass extends Controller
     {
         return view('admin.body.change_password');
     }
-    public function UpdatePassword()
+    public function UpdatePassword(Request $request)
     {
-        //
+        $validateData = $request->validate([
+            'oldpassword'=>'required',
+            'password'=>'required|confirmed',
+        ]);
+        $hashedPassword = Auth::user()->password;
+        if (Hash::check($request->oldpassword, $hashedPassword))
+        {
+            $user = User::find(Auth::id());
+            $user->password = Hash::make($request->password);
+            $user->save();
+            Auth::logout();
+            return redirect()->route('login')->with('success','Ваш пароль был успешно обновлён.');
+        }
+        else {
+            return redirect()->back()->with('error','Введённый пароль не является верным.');
+        }
     }
 }
